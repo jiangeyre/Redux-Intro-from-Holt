f5185e4b95c9157c5944626497ee600f6eb928f7
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-12-08 14:12:47 -0800

    Commit 2

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import reducer from './reducers';
 const store = createStore(
     reducer,
 
-    typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ =/= "undefined"
+    typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
     ? window.__REDUX_DEVTOOLS_EXTENSION__()
     : f => f
 
